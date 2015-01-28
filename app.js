afd491f80c7970f78c7db8e660ec5ad906885f61
anand-seenivasagam <anand.seeni@a-cti.com>
2015-01-28 23:25:59 +0530

    added conditions in binding.gyp for importing c modules

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var mc = require("mac-control");
 // press a on the Mac running node
 mc.keyPress("a");
 
-// press q
-mc.keyPress(12);
+// // press q
+// mc.keyPress(12);
 
-// holds shift, then presses b+c, effectively typing BC, then releases shift
-mc.keyHold("shift");
+// // holds shift, then presses b+c, effectively typing BC, then releases shift
+// mc.keyHold("shift");
 
-    mc.keyPress(["b","c"]);
+//     mc.keyPress(["b","c"]);
 
-mc.keyRelease("shift");
+// mc.keyRelease("shift");
